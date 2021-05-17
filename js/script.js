8b7396596e88ad7c8e4c3758c333ef6ab4cf8892
max <max@verygps.com.tw>
2021-05-17 17:59:35 +0800

    correct switchTheme func to have local storage func

--- js/script.js
+++ js/script.js
@@ -16,14 +16,6 @@ $(function () {
     function darkModeSwitchToggle() {
         const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]')
 
-        function switchTheme(e) {
-            if (e.target.checked) {
-                document.documentElement.setAttribute('data-theme', 'dark')
-            } else {
-                document.documentElement.setAttribute('data-theme', 'light')
-            }
-        }
-
         toggleSwitch.addEventListener('change', switchTheme, false)
 
         function switchTheme(e) {
